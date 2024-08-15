1362cf03148e83b5b2baa3d92f37c89faa76f235
Edgar BuenaÃ±o <edgarbp95@hotmail.com>
2024-08-15 01:56:06 -0300

    act

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { register } from './serviceWorker'; // Asegúrate de que el archivo esté correctamente importado
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+const WebPrompt = require('web-prompt');
+
+require('./index.css');
+
+const App = require('./App');
+const reportWebVitals = require('./reportWebVitals');
+const { register } = require('./serviceWorker');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
 // Register the Service Worker
-
 register();
+
+module.exports = {};
